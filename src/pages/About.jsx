6a088355b9a5cccfc5eba9c4140c072f3dbec35d
MuhZaidan <nadiaz16.mz@gmail.com>
2025-03-08 12:25:41 +0700

    add page about, listrik, and images folder

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,50 @@ import Layout from '../components/Layout';
 const About = () => {
   return (
     <Layout>
-      <h1>About Page</h1>
+      <div className="Article">
+        <div className="details">
+          <div className="title">About Robot</div>
+          <article>
+            Robot line follower pengantar barang merupakan salah satu inovasi
+            teknologi yang memanfaatkan prinsip robotika dan otomatisasi untuk
+            keperluan transportasi barang secara efisien. Robot ini dirancang
+            untuk mengikuti jalur tertentu yang biasanya ditandai dengan garis
+            pada permukaan lantai. Dengan menggunakan sensor, seperti sensor
+            inframerah atau sensor optik, robot ini mampu mendeteksi dan
+            mengikuti jalur tersebut secara mandiri.
+          </article>
+          <div className="subheader">Alasan Pembuatan</div>
+          <article>
+            Pembuatan robot ini didorong oleh kebutuhan untuk meningkatkan
+            efisiensi dalam pengangkutan barang di berbagai lingkungan, seperti
+            gudang, pabrik, atau bahkan area komersial. Penggunaan robot ini
+            dapat membantu mengurangi ketergantungan pada tenaga kerja manusia,
+            meminimalkan kesalahan operasional, dan meningkatkan produktivitas.
+            Selain itu, robot ini juga sering diadopsi sebagai solusi ramah
+            lingkungan karena bisa dirancang untuk menggunakan energi listrik
+            yang lebih hemat dan bersih dibandingkan kendaraan berbahan bakar
+            fosil.
+          </article>
+          <div className="subheader">Cara Kerja</div>
+          <article>
+            Cara kerja dari robot ini yaitu dengan kita menentukan robot harus
+            berpindah kemana melalui website yang telah kami buat, setelah itu
+            pengguna (user) harus menaruhkan barang apa yang akan dikirimkan di
+            tempat yang telah di sediakan, dengan maximal bobot adalah 3kg.
+            Bobot harus kami batasi agar tidak merusak robot dan juga agar tidak
+            menurunkan kecepatan dari robot ini.
+          </article>
+        </div>
+        <div className="logo">
+          <img
+            src="./images/robot.png"
+            alt="Trailo Prototypes"
+            className="trailo-pict"
+            width={500}
+            height={500}
+          />
+        </div>
+      </div>
     </Layout>
   );
 };
--- src/pages/Listrik.jsx
+++ src/pages/Listrik.jsx
@@ -3,7 +3,47 @@ import Layout from '../components/Layout';
 const Listrik = () => {
   return (
     <Layout>
-      <h1>Listrik Industri Page</h1>
+      <div className="Article">
+        <div className="details">
+          <div className="title">Teknik Otomasi Industri</div>
+          <article>
+            Teknik Otomasi Industri adalah sebuah bidang teknik yang mempelajari
+            tentang perakitan sistem-sistem di dunia industri yang berbasis
+            otomatis. Otomasi sendiri biasanya digunakan untuk mengendalikan
+            mesin-mesin industri dan kontrol proses untuk menggantikan operator
+            tenaga manusia. Otomasi Industri dapat bekerja di industri, instansi
+            pemerintah dan swasta, lembaga penelitian, konsultan teknik, sektor
+            pendidikan, dan berwirausaha
+          </article>
+          <div className="subheader">Sejarah</div>
+          <ul>
+            <li>1977 – 2012 : LISTRIK INDUSTRI</li>
+            <li>2012 – Saat ini : TEKNIK OTOMASI INDUSTRI</li>
+          </ul>
+          <div className="subheader">Kerja Sama:</div>
+          <article>
+            Teknik Otomasi Industri di SMKN 1 Cimahi telah bekerja sama dengan
+            beberapa industri, diantaranya:
+          </article>
+          <ul>
+            <li>INDOFOOD CBP SUKSES MAKMUR Tbk</li>
+            <li>PLN (PERSERO)</li>
+            <li>FORSTA KALMEDIC GLOBAL</li>
+            <li>NUTRIFOOD INDONESIA</li>
+            <li>ARSY SOLUTION</li>
+            <li>SAYAP MAS UTAMA (WINGS GROUP)</li>
+            <li>MULTI INDOMANDIRI (WINGS GROUP)</li>
+            <li>WIRA KUSUMA SEJAHTERA</li>
+            <li>CAPRIFARMINDO LABORATORIES</li>
+            <li>PT. GRANITOR SYSTEM APAC</li>
+            <li>PT. JAYA KENCANA</li>
+            <li>POLITEKNIK MANUFAKTUR NEGERI BANDUNG</li>
+          </ul>
+        </div>
+        <div className="logo">
+          <img src="./images/toi.png" alt="Teknik Otomasi Industri" />
+        </div>
+      </div>
     </Layout>
   );
 };
